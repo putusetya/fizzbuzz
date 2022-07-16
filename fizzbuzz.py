42ffe614627dc6b3234384c2fc76ac8724393984
putusetya <root@LAPTOP-OL1VISJT.localdomain>
2022-07-16 14:17:40 +0700

    Menambahkan file daribranchalternate1.txt

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# menerima input angka dari user 
+n=int(input("Masukkan angka: "))
+
+# menjalankan perintah loop dari 1 sampai dengan n
+for i in range(1,n+1):
+    if i%15 == 0:
+        print("FizzBuzz")
+    elif i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(str(i))
+
+
+
